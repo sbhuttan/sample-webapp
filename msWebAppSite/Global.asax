a06974a1d0a1158ddf41a3c79857aa1c61568cf9
Sumit K Bhuttan <sumitbhuttan@gmail.com>
2023-10-20 13:49:47 -0400

    Add files via upload

---
+++ msWebAppSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="msWebAppSite.MvcApplication" Language="C#" %>
---
+++ msWebAppSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(msWebAppSite.Startup))]
+namespace msWebAppSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
